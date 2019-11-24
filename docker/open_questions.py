ff97dfaf422b34fa8b752f93693e9e3f98835d17
Brian Yan <brianyan918@gmail.com>
2019-11-23 19:52:57 -0500

    more changes to questions

--- docker/open_questions.py
+++ docker/open_questions.py
@@ -17,7 +17,7 @@ class QuestionGenerator(object):
         self.questions = []
 
         self.entity_to_pronoun = {'PERSON': 'Who', 'NORP': 'Who', 'FAC': 'Where', 'ORG': 'Which organization',
-                             'GPE': 'Which entity', 'LOC': 'Where', 'PRODUCT': 'What', 'EVENT': 'What event',
+                             'GPE': 'Where', 'LOC': 'Where', 'PRODUCT': 'What', 'EVENT': 'What event',
                              'WORK_OF_ART': 'What', 'LAW': 'What', 'LANGUAGE': 'What language', 'DATE': 'When',
                              'TIME': 'When', 'PERCENT': 'What percent', 'MONEY': 'How much', 'QUANTITY': 'How much',
                             'ORDINAL':'When', 'CARDINAL': 'How many'}
@@ -25,6 +25,10 @@ class QuestionGenerator(object):
         # Parse the document with spaCy
         doc = nlp(article)
         doc = nlp(doc._.coref_resolved)
+        self.ner = {}
+        for ent in doc.ents:
+            self.ner.update({ent.text : ent.label_})
+            #print(ent.text, ent.start_char, ent.end_char, ent.label_)
 
         # Extract semi-structured statements
         svos = textacy.extract.subject_verb_object_triples(doc)
@@ -54,7 +58,7 @@ class QuestionGenerator(object):
 
                 # the relative pronoun is guaranteed to be the left child
                 # right child is direct object
-                verb_phrase = doc[token.left_edge.i + 1 : token.right_edge.i + 1].text # https://spacy.io/usage/examples#subtrees
+                """verb_phrase = doc[token.left_edge.i + 1 : token.right_edge.i + 1].text # https://spacy.io/usage/examples#subtrees
                 verb_phrase = ' '.join(verb_phrase.split())
                 interrogative_pronoun = token.left_edge.text
                 if token.left_edge.text == "which":
@@ -74,7 +78,7 @@ class QuestionGenerator(object):
                 # TODO - coreference for appositives (e.g. "Jacobo, the advisor who retired - replace advisor with Jacobo")
 
                 self.questions.append(x_who_question)
-
+"""
                 test_1 = "Colonel Sanders, who founded KFC, is my hero."
                 test_2 = "Water, which is the source of all life, is made of hydrogen."
                 test_3 = "Jacobo, the advisor who retired, used to teach 15-121."
@@ -88,34 +92,48 @@ class QuestionGenerator(object):
             if token_verb.dep_ == 'ROOT':
                 for token_subj in token_verb.lefts:
                     if token_subj.dep_ == 'nsubj':
+                        if token_subj.lefts:
+                            subj_left = ' '.join([t.text for t in token_subj.lefts])+' '
+                        else:
+                            subj_left = ''
+                        if token_subj.rights:
+                            subj_right = ' ' + ' '.join([t.text for t in token_subj.rights])
+                        else:
+                            subj_right = ''
+                        token_subj_txt = (subj_left + token_subj.text + subj_right).strip()
                         if token_verb.lemma_ not in self.treated_verbs:
                             self.treated_verbs.append(token_verb.lemma_)
-                            statements = textacy.extract.semistructured_statements(doc, token_subj.text, cue=token_verb.lemma_,
+                            statements = textacy.extract.semistructured_statements(doc, token_subj_txt, cue=token_verb.lemma_,
                                                                ignore_entity_case=True)
                             
                             for statement in statements:
                                 entity, verb, fact = statement
-                                #fact = ' '.join(fact.text.split('\n'))
-                                self.questions.append(self.generate_open_question(entity, verb, fact, token_verb.lemma_))
-                                #print(1, entity, verb, fact.text.strip())
-                                    
+                                q = self.generate_open_question(entity, verb, fact, token_verb.lemma_)
+                                if q:
+                                    self.questions.append(q)
                                 self.closed_questions.append(self.generate_closed_question(entity, verb, fact, token_verb.lemma_))
 
     
     def generate_open_question(self, entity, verb, fact, lemma):
-        entity_txt = ' '.join(entity.text.split()).strip()
+        if entity.text not in self.ner and entity.text != "I":
+            entity_txt = entity.text.lower()
+            entity_txt = ' '.join(entity_txt.split()).strip()
+        else:
+            entity_txt = ' '.join(entity.text.split()).strip()
         fact_txt = ' '.join(fact.text.split()).strip()
         verb_txt = ' '.join(verb.text.split()).strip()
         if (fact_txt[-1] == '.'):
-            fact_txt[:-1]
-
+            fact_txt = fact_txt[:-1]
+        # print(entity.text, entity_txt, verb.text, fact.text)
         if entity.label_ in self.entity_to_pronoun:
             w_word = self.entity_to_pronoun[entity.label_]
+        elif entity_txt in self.ner:
+            w_word = self.entity_to_pronoun[self.ner[entity_txt]]
         else:
-            w_word = "What"
+            return
         if (' ' in verb.text):
             verb = verb.text.split()
-            question = w_word + verb[0] + ' ' + entity_txt + ' ' + verb[1] + '?'
+            question = w_word + ' ' + verb[0] + ' ' + entity_txt + ' ' + verb[1] + '?'
         elif lemma == "be":
             # TODO: tense / agreement
             #question = "What is " + fact.text.strip() + '?'
@@ -126,26 +144,38 @@ class QuestionGenerator(object):
             # TODO: if entity is a named entity (who) / time (when) / location (where) - what/why/who/when
         else: # X verbs Y
             #question = "What does " + entity.text + ' ' + verb.text + '?'
-            if verb.tag_ == "VBD":
-                question = w_word + " did " + entity_txt + ' ' + lemma + " " + fact_txt + '?'
+            """if verb.tag_ == "VBD":
+                question = w_word + " did "
             elif verb.tag_ == "VBZ":
-                question = w_word + " does " + entity_txt + ' ' + lemma + " " + fact_txt + '?'
+                question = w_word + " does "
             elif verb.tag_ == "VBP":
-                question = w_word + " do " + entity_txt + ' ' + lemma + " " + fact_txt + '?'
+                question = w_word + " do "
             else:
-                question = w_word + " did " + entity_txt + ' ' + lemma + " " + fact_txt + '?'
-
+                question = w_word + " did "
+            if entity[0].dep_ != 'nsubjpass' and entity[0].dep_ != 'auxpass':
+                question += entity_txt
+            question += ' ' + lemma + " " + fact_txt + '?'"""
+            if verb_txt == "are ":
+                verb_txt = "is"
+            #elif verb_txt.split()[0] == "have":
+                #verb_txt = "has" + ' '.join(verb_txt.split()[1:])
+            verb_txt = verb_txt.replace("have ", "has ")
+            question = w_word + ' ' + verb_txt + ' ' + fact_txt
 
         # Capitalize first letter of string
         #print(question)
-        return question.capitalize()
+        return question
 
     def generate_closed_question(self, entity, verb, fact, lemma):
-        entity_txt = ' '.join(entity.text.split()).strip()
+        if entity.text not in self.ner and entity.text != "I":
+            entity_txt = entity.text.lower()
+            entity_txt = ' '.join(entity_txt.split()).strip()
+        else:
+            entity_txt = ' '.join(entity.text.split()).strip()
         fact_txt = ' '.join(fact.text.split()).strip()
         verb_txt = ' '.join(verb.text.split()).strip()
         if (fact_txt[-1] == '.'):
-            fact_txt[:-1]
+            fact_txt = fact_txt[:-1]
         if (' ' in verb_txt): # auxiliary verbs
             verb_txt = verb_txt.split()
             question = verb_txt[0] + ' ' + entity_txt + ' ' + verb_txt[1] + ' ' + fact_txt + '?'
